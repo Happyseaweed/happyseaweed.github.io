7915b03adff836b0dc1eec5cdb0a280ee8b6a5d0
Siwei Du <seaweedgo@gmail.com>
2024-04-16 23:29:27 -0400

    about me UI redesign

--- skills.js
+++ skills.js
@@ -45,10 +45,13 @@ document.addEventListener("DOMContentLoaded", function () {
 
 document.addEventListener("DOMContentLoaded", function () {
   const headers = document.querySelectorAll(".collapsible-header");
+  const collapsible = document.querySelectorAll(".collapsible-header-container");
+
+  console.log(collapsible);
 
   headers.forEach((header) => {
     header.addEventListener("click", function () {
-      const content = this.nextElementSibling;
+      const content = this.parentElement.nextElementSibling;
       if (content.style.display === "block") {
         content.style.display = "none";
       } else {
